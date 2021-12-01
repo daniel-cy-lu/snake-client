234b039b386d4ad0948db2d7bb94633adf06cde8
Daniel Lu <daniel_lu@hotmail.com>
2021-12-01 15:55:55 -0500

    name displayed on snake when client is connected

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
@@ -6,12 +7,14 @@ const connect = function () {
     port: 50541
   });
 
+  // name is display on snake when connected to server
   conn.on('connect', () => {
     conn.write('Name: DLU'); 
   })
 
   conn.setEncoding("utf8");
   
+  // return data when the server kicks user out
   conn.on('data', (data) => {
     console.log(data);
   });
