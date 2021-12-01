e504d3538a4a26881d8a4ec2bb7ee6e666477f71
Daniel Lu <daniel_lu@hotmail.com>
2021-12-01 17:15:37 -0500

    refactor by splitting part of play.js to input.js

--- play.js
+++ play.js
@@ -1,22 +1,11 @@
 const connect = require('./client');
-
-// setup interface to handle user input from stdin
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
+const input = require('./input');
 
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
 console.log("Connecting ...");
+// call connect func from client.js
 connect();
+// call setupInput func from input.js
+input();
 
